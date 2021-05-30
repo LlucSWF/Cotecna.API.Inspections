32c519c64872ca58dad6e652eadef5702f083eca
LlucSWF <lluc724@gmail.com>
2021-05-30 23:20:47 +0200

    Delete InspectionsDatabase directory

--- InspectionsDatabase/Inspection.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Inspection]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Rating] DECIMAL(2, 2) NULL, 
-    [ScheduledDate] DATETIME NOT NULL, 
-    [InspectorId] INT NOT NULL
-    FOREIGN KEY (InspectorId) REFERENCES InspectorInfo(Id)
-)
--- InspectionsDatabase/InspectorInfo.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[InspectorInfo]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NOT NULL, 
-    [Surnames] NVARCHAR(100) NULL, 
-    [NIF] NCHAR(9) NOT NULL
-)
